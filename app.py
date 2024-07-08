a17fc80c9c4b15e1c2c514df889d33a7df450e49
Kevin Jiang <kevinjiang121@gmail.com>
2024-07-08 19:36:33 -0400

    Add basic text based graph with basic functionalities.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, send_file
+import json
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -17,6 +19,17 @@ class Graph:
             self.edges[node1].append(node2)
             self.edges[node2].append(node1)
 
+    def to_dict(self):
+        return {"nodes": self.nodes, "edges": self.edges}
+
+    def from_dict(self, data):
+        self.nodes = data["nodes"]
+        self.edges = data["edges"]
+
+    def clear(self):
+        self.nodes = []
+        self.edges = {}
+
 graph = Graph()
 
 @app.route('/')
@@ -34,6 +47,24 @@ def add_edge():
     node1 = request.form['node1']
     node2 = request.form['node2']
     graph.add_edge(node1, node2)
+    return redirect(url_for('index'))
+
+@app.route('/export', methods=['GET'])
+def export_graph():
+    graph_json = json.dumps(graph.to_dict())
+    return send_file(BytesIO(graph_json.encode()), mimetype='application/json', as_attachment=True, download_name='graph.json')
+
+@app.route('/import', methods=['POST'])
+def import_graph():
+    file = request.files['file']
+    if file:
+        data = json.load(file)
+        graph.from_dict(data)
+    return redirect(url_for('index'))
+
+@app.route('/clear', methods=['POST'])
+def clear_graph():
+    graph.clear()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
